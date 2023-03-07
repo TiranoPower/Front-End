2373957b60342000bb4ce57f5f86e7fbc5d20d89
Jun <122006463+TiranoPower@users.noreply.github.com>
2023-03-07 21:27:44 +0900

    Create readme.d

---
+++ Study/readme.d
@@ -0,0 +1,40 @@
+1. Web Tutorial(HTML,CSS,JS)
+https://www.w3schools.com/
+http://poiemaweb.com
+
+
+2. CSS
+* 색
+https://m2.material.io/design/color/the-color-system.html#tools-for-picking-colors
+https://www.w3schools.com/colors/colors_names.asp
+https://color.adobe.com/ko/explore
+
+*글귀
+https://fonts.google.com/specimen/Black+Han+Sans?subset=korean&sidebar.open=true&selection.family=Black+Han+Sans
+
+* 정규표현식
+https://www.w3bai.com/ko/tags/att_input_pattern.html#gsc.tab=0
+
+* board-style, radius
+https://developer.mozilla.org/ko/docs/Web/CSS/border-style
+https://developer.mozilla.org/ko/docs/Web/CSS/border-radius
+
+* 트랜지션 연습
+https://dbaron.org/css/timing-function-graphs
+
+* 각도(transform-origin)
+https://developer.mozilla.org/en-US/docs/Web/CSS/transform-origin
+
+* 명시도 계산
+https://specificity.keegan.st/
+
+* Flexbox 연습
+https://flexbox.tech/
+
+* Flexbox 테스트
+https://flexboxfroggy.com/#ko
+
+
+
+3. 로드맵
+https://github.com/kamranahmedse/developer-roadmap
